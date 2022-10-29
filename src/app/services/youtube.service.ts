9ac044a2be162b027f72546889ee04d1b21c74b5
nurulaffh <112611536+nurulaffh@users.noreply.github.com>
2022-10-29 18:39:35 +0800

    Update youtube.service.ts

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -15,7 +15,7 @@ export class YoutubeService {
   constructor (private http:HttpClient) {}
 
   getChannels(channelName: string):Observable<any>{
-    const API_KEY ="AIzaSyCH9m2OnKH_3suQA-l6JmuPt6krQGGpjTc"
+    const API_KEY ="[API KET]=get it from youtube developer"
     const url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" +channelName+"&type=channel&key=" +API_KEY+ "&maxResults50"
     return this.http.get<any>(url)
   }
